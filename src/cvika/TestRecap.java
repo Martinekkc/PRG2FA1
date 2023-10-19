ceebccd0b990317e540769a9736d0daefab3bcc4
martin.cerny <martin.cerny@infis.cz>
2023-10-19 13:06:20 +0200

    Provedena zmena

--- src/cvika/TestRecap.java
+++ src/cvika/TestRecap.java
@@ -6,18 +6,36 @@ public class TestRecap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadej pocet lahvi:");
-        int count = sc.nextInt();
-        int pricePerBottle = 540;
-        int finalPrice = 0;
-        if (count < 5){
-            finalPrice = count * pricePerBottle;
-        } else if(count < 10){
-            finalPrice = (int)(count * pricePerBottle * 0.9);
-        } else if (count < 50) {
-            finalPrice = (int)(count * pricePerBottle * 0.8);
-        } else {
-            System.out.println("Takove mnozsvi koupit nemuzes");
+        int input = sc.nextInt();
+//        while (input % 2 == 0) {
+//            System.err.println(input);
+//        }
+//        input = sc.nextInt();
+//    }
+//
+//    System.out.println("Zadej max cislo");
+//    input = sc.nextInt();
+//    for (int i = 0; i < input; i++) {
+//        if (i % 2 == 0){
+//            System.out.println(i);
+//        }
+//    }
+//
+//
+//
+//}
+//
+//
+//
+        System.out.println("Zadej cislo");
+        input = sc.nextInt();
+        boolean prime = true;
+        for (int i = 2; i < input; i++) {
+            if (input % i == 0) {
+                prime = false;
+                break;
+            }
         }
-        System.out.println("Celkova cena je " + finalPrice);
+        System.out.println("Je prvocislo? " + prime);
+
     }
-}
